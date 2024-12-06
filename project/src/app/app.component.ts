7a2f6518d0940813f98bf5bd85975a7f85ce4604
LucasHBD <demetrio.lucas@academico.ifrn.edu.br>
2024-12-06 15:24:35 -0300

    :sparkles: feat: Implementação da Barra Lateral

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  open: boolean = false;
+  title = 'Dashboard';
+  faBars = faBars;
+
+  toggleMenu(){
+    this.open = ! this.open;
+  }
+
 }
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    AppComponent,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
